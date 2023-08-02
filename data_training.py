3c81ff6f24266bfbcf89792f37320a905958d13e
Shivam Jaiswal <76699472+shivam-jaiswal-2002@users.noreply.github.com>
2023-08-02 19:20:52 +0530

    data_training.py

--- data_training.py
+++ data_training.py
@@ -1,64 +1,68 @@
-import mediapipe as mp 
+import os  
 import numpy as np 
 import cv2 
+from tensorflow.keras.utils import to_categorical
+
+from keras.layers import Input, Dense 
+from keras.models import Model
  
-cap = cv2.VideoCapture(0)
+is_init = False
+size = -1
 
-name = input("Enter the name of the data : ")
+label = []
+dictionary = {}
+c = 0
 
-holistic = mp.solutions.holistic
-hands = mp.solutions.hands
-holis = holistic.Holistic()
-drawing = mp.solutions.drawing_utils
+for i in os.listdir():
+	if i.split(".")[-1] == "npy" and not(i.split(".")[0] == "labels"):  
+		if not(is_init):
+			is_init = True 
+			X = np.load(i)
+			size = X.shape[0]
+			y = np.array([i.split('.')[0]]*size).reshape(-1,1)
+		else:
+			X = np.concatenate((X, np.load(i)))
+			y = np.concatenate((y, np.array([i.split('.')[0]]*size).reshape(-1,1)))
 
-X = []
-data_size = 0
-
-while True:
- lst = []
-
- _, frm = cap.read()
-
- frm = cv2.flip(frm, 1)
-
- res = holis.process(cv2.cvtColor(frm, cv2.COLOR_BGR2RGB))
-
- if res.face_landmarks:
-  for i in res.face_landmarks.landmark:
-   lst.append(i.x - res.face_landmarks.landmark[1].x)
-   lst.append(i.y - res.face_landmarks.landmark[1].y)
-
-  if res.left_hand_landmarks:
-   for i in res.left_hand_landmarks.landmark:
-    lst.append(i.x - res.left_hand_landmarks.landmark[8].x)
-    lst.append(i.y - res.left_hand_landmarks.landmark[8].y)
-  else:
-   for i in range(42):
-    lst.append(0.0)
-
-  if res.right_hand_landmarks:
-   for i in res.right_hand_landmarks.landmark:
-    lst.append(i.x - res.right_hand_landmarks.landmark[8].x)
-    lst.append(i.y - res.right_hand_landmarks.landmark[8].y)
-  else:
-   for i in range(42):
-    lst.append(0.0)
+		label.append(i.split('.')[0])
+		dictionary[i.split('.')[0]] = c  
+		c = c+1
 
 
-  X.append(lst)
-  data_size = data_size+1
+for i in range(y.shape[0]):
+	y[i, 0] = dictionary[y[i, 0]]
+y = np.array(y, dtype="int32")
 
- drawing.draw_landmarks(frm, res.face_landmarks, holistic.FACEMESH_CONTOURS)
- drawing.draw_landmarks(frm, res.left_hand_landmarks, hands.HAND_CONNECTIONS)
- drawing.draw_landmarks(frm, res.right_hand_landmarks, hands.HAND_CONNECTIONS)
+###  hello = 0 nope = 1 ---> [1,0] ... [0,1]
 
- cv2.putText(frm, str(data_size), (50,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0),2)
+y = to_categorical(y)
 
- cv2.imshow("window", frm)
- if cv2.waitKey(1) == 27 or data_size>99:
-  cv2.destroyAllWindows()
-  cap.release()
-  break
+X_new = X.copy()
+y_new = y.copy()
+counter = 0 
 
-np.save(f"{name}.npy", np.array(X))
-print(np.array(X).shape)
+cnt = np.arange(X.shape[0])
+np.random.shuffle(cnt)
+
+for i in cnt: 
+	X_new[counter] = X[i]
+	y_new[counter] = y[i]
+	counter = counter + 1
+
+
+ip = Input(shape=(X.shape[1]))
+
+m = Dense(512, activation="relu")(ip)
+m = Dense(256, activation="relu")(m)
+
+op = Dense(y.shape[1], activation="softmax")(m) 
+
+model = Model(inputs=ip, outputs=op)
+
+model.compile(optimizer='rmsprop', loss="categorical_crossentropy", metrics=['acc'])
+
+model.fit(X, y, epochs=50)
+
+
+model.save("model.h5")
+np.save("labels.npy", np.array(label))
