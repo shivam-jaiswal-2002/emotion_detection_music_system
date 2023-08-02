f2a69a29d26701de610a6d4c3e0977a43083da6e
Shivam Jaiswal <76699472+shivam-jaiswal-2002@users.noreply.github.com>
2023-08-02 19:21:50 +0530

    inference.py

--- inference.py
+++ inference.py
@@ -3,8 +3,11 @@ import numpy as np
 import mediapipe as mp 
 from keras.models import load_model 
 
-model = load_model("model.h5")
+
+model  = load_model("model.h5")
 label = np.load("labels.npy")
+
+
 
 holistic = mp.solutions.holistic
 hands = mp.solutions.hands
@@ -13,47 +16,54 @@ drawing = mp.solutions.drawing_utils
 
 cap = cv2.VideoCapture(0)
 
+
+
 while True:
- lst = []
+	lst = []
 
- _, frm = cap.read()
- frm = cv2.flip(frm, 1)
- res = holis.process(cv2.cvtColor(frm, cv2.COLOR_BGR2RGB))
+	_, frm = cap.read()
 
- if res.face_landmarks:
-  for i in res.face_landmarks.landmark:
-   lst.append(i.x - res.face_landmarks.landmark[1].x)
-   lst.append(i.y - res.face_landmarks.landmark[1].y)
+	frm = cv2.flip(frm, 1)
 
-  if res.left_hand_landmarks:
-   for i in res.left_hand_landmarks.landmark:
-    lst.append(i.x - res.left_hand_landmarks.landmark[8].x)
-    lst.append(i.y - res.left_hand_landmarks.landmark[8].y)
-  else:
-   for i in range(42):
-    lst.append(0.0)
-if res.right_hand_landmarks:
-   for i in res.right_hand_landmarks.landmark:
-    lst.append(i.x - res.right_hand_landmarks.landmark[8].x)
-    lst.append(i.y - res.right_hand_landmarks.landmark[8].y)
-  else:
-   for i in range(42):
-    lst.append(0.0)
+	res = holis.process(cv2.cvtColor(frm, cv2.COLOR_BGR2RGB))
 
-  lst = np.array(lst).reshape(1,-1)
 
-  pred = label[np.argmax(model.predict(lst))]
+	if res.face_landmarks:
+		for i in res.face_landmarks.landmark:
+			lst.append(i.x - res.face_landmarks.landmark[1].x)
+			lst.append(i.y - res.face_landmarks.landmark[1].y)
 
-  print(pred)
-  cv2.putText(frm, pred, (50,50),cv2.FONT_ITALIC, 1, (255,0,0),2)
+		if res.left_hand_landmarks:
+			for i in res.left_hand_landmarks.landmark:
+				lst.append(i.x - res.left_hand_landmarks.landmark[8].x)
+				lst.append(i.y - res.left_hand_landmarks.landmark[8].y)
+		else:
+			for i in range(42):
+				lst.append(0.0)
 
- drawing.draw_landmarks(frm, res.face_landmarks, holistic.FACEMESH_CONTOURS)
- drawing.draw_landmarks(frm, res.left_hand_landmarks, hands.HAND_CONNECTIONS)
- drawing.draw_landmarks(frm, res.right_hand_landmarks, hands.HAND_CONNECTIONS)
+		if res.right_hand_landmarks:
+			for i in res.right_hand_landmarks.landmark:
+				lst.append(i.x - res.right_hand_landmarks.landmark[8].x)
+				lst.append(i.y - res.right_hand_landmarks.landmark[8].y)
+		else:
+			for i in range(42):
+				lst.append(0.0)
 
- cv2.imshow("window", frm)
+		lst = np.array(lst).reshape(1,-1)
 
- if cv2.waitKey(1) == 27:
-  cv2.destroyAllWindows()
-  cap.release()
-  break
+		pred = label[np.argmax(model.predict(lst))]
+
+		print(pred)
+		cv2.putText(frm, pred, (50,50),cv2.FONT_ITALIC, 1, (255,0,0),2)
+
+		
+	drawing.draw_landmarks(frm, res.face_landmarks, holistic.FACEMESH_CONTOURS)
+	drawing.draw_landmarks(frm, res.left_hand_landmarks, hands.HAND_CONNECTIONS)
+	drawing.draw_landmarks(frm, res.right_hand_landmarks, hands.HAND_CONNECTIONS)
+
+	cv2.imshow("window", frm)
+
+	if cv2.waitKey(1) == 27:
+		cv2.destroyAllWindows()
+		cap.release()
+		break
